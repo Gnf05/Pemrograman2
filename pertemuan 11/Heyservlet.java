a79091ffa66d3b2ec82c9134e8301f63185cbb85
Gnf05 <galihnaufal194@gmail.com>
2026-06-05 16:02:36 +0700

    Update Heyservlet.java

--- pertemuan 11/Heyservlet.java
+++ pertemuan 11/Heyservlet.java
@@ -1,1 +1,26 @@
+// Nama : Galih Naufal Faturrohman
+// Nim : 231011402731
+// Kelas : 06TPLE016
 
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hey Servlet!</h1>");
+    }
+}
